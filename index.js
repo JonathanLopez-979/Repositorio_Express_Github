491a4fc3ea4abc3c9297b9b31cd5a6f945dd52b4
Alumno de la Facultad de Informatica Mazatlan <alumno@fimaz.local>
2026-04-13 10:16:01 -0700

    Primer SELECT de alumnos con PosgreSql y Express

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
-const pool = require('./db');
+const pool = require('./db'); // Aquí importa tu archivo db.js
 const app = express();
 
+app.use(express.json());
+
+// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
 
-pool.connect()
-  .then(() => {
-    console.log('Conexión exitosa a PostgreSQL');
-  })
-  .catch((err) => {
-    console.error('Error de conexión', err);
-  });
+// La ruta que te pide la Parte 3
+app.get('/alumnos', async (req, res) => {
+  try {
+    const resultado = await pool.query('SELECT * FROM alumno');
+    res.json(resultado.rows);
+  } catch (error) {
+    console.error('Error al consultar alumnos:', error);
+    res.status(500).json({ error: 'Error al obtener los alumnos' });
+  }
+});
 
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://S');
+  console.log('Servidor corriendo en http://localhost:3000');
 });
